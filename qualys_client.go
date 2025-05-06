75e2a6e04f59691fa1b8c99d5f95ef06ee18aacd
zany2dmax <hikerguy@jeffleggett.com>
2025-05-06 13:23:04 -0400

    working qualys API code

--- qualys_client.go
+++ qualys_client.go
@@ -8,56 +8,86 @@ import (
 	"github.com/go-resty/resty/v2"
 )
 
-// Client wraps the Resty client for Qualys Tagging API v2
-type Client struct { r *resty.Client }
+// Client wraps Resty for the QPS Tagging API
+type Client struct{ r *resty.Client }
 
-// NewClient creates a Qualys API client with basic auth
+// NewClient points Resty at your QPS server
 func NewClient(baseURL, username, password string) *Client {
 	r := resty.New().
 		SetHostURL(baseURL).
 		SetBasicAuth(username, password).
-		SetHeader("Content-Type", "application/xml")
+		SetHeader("Content-Type", "text/xml").
+		SetHeader("Accept", "application/xml")
 	return &Client{r: r}
 }
 
-// QualysTag models a single tag from Qualys
-type QualysTag struct { ID int `xml:"TAG_ID"`; Name string `xml:"TAG_NAME"` }
-
-// tagsResponse wraps the XML from Qualys
-type tagsResponse struct {
-	XMLName xml.Name `xml:"ServiceResponse"`
-	Data    struct{ Tags []QualysTag `xml:"data>ROW"` } `xml:"data"`
+// QualysTag is one tag’s ID + name
+type QualysTag struct {
+	ID   int    `xml:"id"`
+	Name string `xml:"name"`
 }
 
-// ListTags retrieves all tags from Qualys
+// tagsResponse matches the <ServiceResponse><data><Tag>… XML
+type tagsResponse struct {
+	XMLName xml.Name `xml:"ServiceResponse"`
+	Data    struct {
+		Tags []QualysTag `xml:"Tag"`
+	} `xml:"data"`
+}
+
+func xmlEscape(s string) string {
+	return strings.ReplaceAll(s, "&", "&amp;")
+}
+
+const searchTagPath = "/qps/rest/2.0/search/am/tag"
+
+// ListTags retrieves *all* tags via a POST to the Search Tags endpoint
 func (qc *Client) ListTags() ([]QualysTag, error) {
+	// Build a body that says “no filters, give me up to 10 000 tags”
+	reqBody := `
+      <ServiceRequest>
+        <preferences>
+          <startFromOffset>1</startFromOffset>
+          <limitResults>1000</limitResults>
+        </preferences>
+      </ServiceRequest>`
 	resp, err := qc.r.R().
-		EnableTrace().
-		SetQueryParams(map[string]string{"action":"list","truncation_limit":"10000"}).
+		SetBody(reqBody).
 		SetResult(&tagsResponse{}).
-		Get("/api/2.0/fo/asset/tag/")
-	if err != nil { return nil, fmt.Errorf("Qualys ListTags failed: %w", err) }
-
- 	fmt.Printf("Qualys replied (HTTP %d):\n%s\n\n", resp.StatusCode(), resp.String())
-
-  result := resp.Result().(*tagsResponse)
+		Post(searchTagPath)
+	if err != nil {
+		return nil, fmt.Errorf("Qualys ListTags failed: %w", err)
+	}
+	if resp.StatusCode() != 200 {
+		return nil, fmt.Errorf("unexpected HTTP %d from Qualys:\n%s",
+			resp.StatusCode(), resp.String())
+	}
+	result := resp.Result().(*tagsResponse)
 	return result.Data.Tags, nil
 }
 
-// UpsertTag creates or updates a tag in Qualys
-type upsertResponse struct{
-	XMLName xml.Name
-	Data    struct{ Count int `xml:"count,attr"` } `xml:"data"`
-}
-
+// UpsertTag creates or updates a tag in Qualys via the QPS Update endpoint.
 func (qc *Client) UpsertTag(t Tag) error {
-	// Qualys add: action=add
-	payload := fmt.Sprintf("<ServiceRequest><data><ROW><TAG_NAME>%s</TAG_NAME></ROW></data></ServiceRequest>", xmlEscape(t.Name))
-	_, err := qc.r.R().
-		SetQueryParam("action", "add").
-		SetBody(payload).
-		Post("/api/2.0/fo/asset/tag/")
-	return err
-}
+	// Build the ServiceRequest XML body.
+	// QPS expects <ServiceRequest><data><Tag>…</Tag></data></ServiceRequest>
+	reqBody := fmt.Sprintf(`
+<ServiceRequest>
+  <data>
+    <Tag>
+      <name>%s</name>
+    </Tag>
+  </data>
+</ServiceRequest>`, xmlEscape(t.Name))
 
-func xmlEscape(s string) string { return strings.ReplaceAll(s, "&", "&amp;") }
+	resp, err := qc.r.R().
+		SetBody(reqBody).
+		Post("/qps/rest/2.0/update/am/tag")
+	if err != nil {
+		return fmt.Errorf("Qualys UpsertTag request failed: %w", err)
+	}
+	if resp.StatusCode() != 200 {
+		return fmt.Errorf("Qualys UpsertTag unexpected HTTP %d: %s",
+			resp.StatusCode(), resp.String())
+	}
+	return nil
+}
